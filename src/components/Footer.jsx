e8c4dc80ba2b426944edecffd3da735dec516bdf
jwhit <28734055+jwhit-commit@users.noreply.github.com>
2023-12-06 15:43:58 -0600

    fixing image links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ function Footer() {
   return (
     <ul className="d-flex justify-content-center pt-5">
         <a href="https://github.com/jwhit-commit">
-            <img src="src/assets/github.jpg" alt="github"></img>
+            <img src="public/assets/github.jpg" alt="github"></img>
         </a>
         <a href="https://www.linkedin.com/in/whitseo/">
-            <img src="src/assets/linkedin.png" alt="linkedin"></img>
+            <img src="public/assets/linkedin.png" alt="linkedin"></img>
         </a>
         <a href="https://www.threads.net/@farqfox">
-            <img src="src/assets/threads.png" alt="threads"></img>
+            <img src="public/assets/threads.png" alt="threads"></img>
         </a>
     </ul>
   );
--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -7,16 +7,16 @@ export default function PortfolioPage() {
                 Portfolio
             </h3>
             <div className="container-fluid d-flex justify-content-center align-items-center my-5">
-                    <a href=""><img src="src/assets/workouttracker.png" alt="workout tracker" className="folio mx-5"></img></a>
-                    <a href="https://github.com/jwhit-commit/jwhit-note-taker"><img src="src/assets/notetaker.png" alt="note taker" className="folio mx-5"></img></a>
+                    <a href=""><img src="public/assets/workouttracker.png" alt="workout tracker" className="folio mx-5"></img></a>
+                    <a href="https://github.com/jwhit-commit/jwhit-note-taker"><img src="public/assets/notetaker.png" alt="note taker" className="folio mx-5"></img></a>
             </div>
             <div className="container-fluid d-flex justify-content-center align-items-center my-5">
-                    <a href="https://secure-plateau-16621-4c6aadcc7f2d.herokuapp.com/"><img src="src/assets/cms.png" alt="blog cms" className="folio mx-5"></img></a>
-                    <a href="https://desolate-cove-89215-d9792a7c3caa.herokuapp.com/"><img src="src/assets/evlocator.png" alt="ev locator" className="folio mx-5"></img></a>
+                    <a href="https://secure-plateau-16621-4c6aadcc7f2d.herokuapp.com/"><img src="public/assets/cms.png" alt="blog cms" className="folio mx-5"></img></a>
+                    <a href="https://desolate-cove-89215-d9792a7c3caa.herokuapp.com/"><img src="public/assets/evlocator.png" alt="ev locator" className="folio mx-5"></img></a>
             </div>
             <div className="container-fluid d-flex justify-content-center align-items-center my-5">
-                    <a href="https://github.com/jwhit-commit/jwhit-readme-generator"><img src="src/assets/readmegenerator.png" alt="readme generator" className="folio mx-5"></img></a>
-                    <a href="https://jwhit-commit.github.io/jwhit-cha4-web-quiz/"><img src="src/assets/timedquiz.png" alt="folio" className="folio mx-5"></img></a>
+                    <a href="https://github.com/jwhit-commit/jwhit-readme-generator"><img src="public/assets/readmegenerator.png" alt="readme generator" className="folio mx-5"></img></a>
+                    <a href="https://jwhit-commit.github.io/jwhit-cha4-web-quiz/"><img src="public/assets/timedquiz.png" alt="folio" className="folio mx-5"></img></a>
             </div>
         </div>
       );
